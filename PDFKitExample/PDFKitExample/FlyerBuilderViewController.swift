b725804d80e552b81865332ca5567b3510f49122
songjucho <pinetree134@ajou.ac.kr>
2021-04-07 18:51:42 +0900

    feat: preview 버튼 눌렀을 때 화면 전환되며 pdf 생성하는 prepare 함수 추가

--- PDFKitExample/PDFKitExample/FlyerBuilderViewController.swift
+++ PDFKitExample/PDFKitExample/FlyerBuilderViewController.swift
@@ -1,83 +1,93 @@
 import UIKit
 
 class FlyerBuilderViewController: UIViewController {
-  @IBOutlet weak var scrollView: UIScrollView!
-  @IBOutlet weak var flyerTextEntry: UITextField!
-  @IBOutlet weak var bodyTextView: UITextView!
-  @IBOutlet weak var contactTextView: UITextView!
-  @IBOutlet weak var imagePreview: UIImageView!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var flyerTextEntry: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var contactTextView: UITextView!
+    @IBOutlet weak var imagePreview: UIImageView!
     
-    // Add subtle outline around text views
-    bodyTextView.layer.borderColor = UIColor.gray.cgColor
-    bodyTextView.layer.borderWidth = 1.0
-    bodyTextView.layer.cornerRadius = 4.0
-    contactTextView.layer.borderColor = UIColor.gray.cgColor
-    contactTextView.layer.borderWidth = 1.0
-    contactTextView.layer.cornerRadius = 4.0
-    
-    // Add the share icon and action
-    let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareAction))
-    self.toolbarItems?.append(shareButton)
-    
-    // Add responder for keyboards to dismiss when tap or drag outside of text fields
-    scrollView.addGestureRecognizer(UITapGestureRecognizer(target: scrollView, action: #selector(UIView.endEditing(_:))))
-    scrollView.keyboardDismissMode = .onDrag
-  }
-  
-  @objc func shareAction() {
-    print("Share icon touched.")
-  }
-  
-  @IBAction func selectImageTouched(_ sender: Any) {
-    let actionSheet = UIAlertController(title: "Select Photo", message: "Where do you want to select a photo?", preferredStyle: .actionSheet)
-    
-    let photoAction = UIAlertAction(title: "Photos", style: .default) { (action) in
-      if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
-        let photoPicker = UIImagePickerController()
-        photoPicker.delegate = self
-        photoPicker.sourceType = .photoLibrary
-        photoPicker.allowsEditing = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        self.present(photoPicker, animated: true, completion: nil)
-      }
-    }
-    actionSheet.addAction(photoAction)
-    
-    let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
-      if UIImagePickerController.isSourceTypeAvailable(.camera){
-        let cameraPicker = UIImagePickerController()
-        cameraPicker.delegate = self
-        cameraPicker.sourceType = .camera
+        // Add subtle outline around text views
+        bodyTextView.layer.borderColor = UIColor.gray.cgColor
+        bodyTextView.layer.borderWidth = 1.0
+        bodyTextView.layer.cornerRadius = 4.0
+        contactTextView.layer.borderColor = UIColor.gray.cgColor
+        contactTextView.layer.borderWidth = 1.0
+        contactTextView.layer.cornerRadius = 4.0
         
-        self.present(cameraPicker, animated: true, completion: nil)
-      }
+        // Add the share icon and action
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareAction))
+        self.toolbarItems?.append(shareButton)
+        
+        // Add responder for keyboards to dismiss when tap or drag outside of text fields
+        scrollView.addGestureRecognizer(UITapGestureRecognizer(target: scrollView, action: #selector(UIView.endEditing(_:))))
+        scrollView.keyboardDismissMode = .onDrag
     }
-    actionSheet.addAction(cameraAction)
     
-    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-    actionSheet.addAction(cancelAction)
+    @objc func shareAction() {
+        print("Share icon touched.")
+    }
     
-    self.present(actionSheet, animated: true, completion: nil)
-  }
+    @IBAction func selectImageTouched(_ sender: Any) {
+        let actionSheet = UIAlertController(title: "Select Photo", message: "Where do you want to select a photo?", preferredStyle: .actionSheet)
+        
+        let photoAction = UIAlertAction(title: "Photos", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+                let photoPicker = UIImagePickerController()
+                photoPicker.delegate = self
+                photoPicker.sourceType = .photoLibrary
+                photoPicker.allowsEditing = false
+                
+                self.present(photoPicker, animated: true, completion: nil)
+            }
+        }
+        actionSheet.addAction(photoAction)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                let cameraPicker = UIImagePickerController()
+                cameraPicker.delegate = self
+                cameraPicker.sourceType = .camera
+                
+                self.present(cameraPicker, animated: true, completion: nil)
+            }
+        }
+        actionSheet.addAction(cameraAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        actionSheet.addAction(cancelAction)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    // preview 버튼 누르면 일어나는 일
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "previewSegue" {
+            guard let vc = segue.destination as? PDFPreviewViewController else { return }
+            let pdfCreator = PDFCreator()
+            // 데이터 주입 - 캡슐화 안해도 되나?
+            vc.documentData = pdfCreator.createFlyer()
+        }
+    }
 }
 
 extension FlyerBuilderViewController: UIImagePickerControllerDelegate {
-  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-    
-    guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-      return
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+        imagePreview.image = selectedImage
+        imagePreview.isHidden = false
+        
+        dismiss(animated: true, completion: nil)
     }
-    
-    imagePreview.image = selectedImage
-    imagePreview.isHidden = false
-    
-    dismiss(animated: true, completion: nil)
-  }
 }
 
 extension FlyerBuilderViewController: UINavigationControllerDelegate {
-  // Not used, but necessary for compilation
+    // Not used, but necessary for compilation
 }
