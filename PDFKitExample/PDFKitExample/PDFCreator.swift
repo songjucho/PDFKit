95d19723d64b89acb570c6c65079f06cdea7d0b7
songjucho <pinetree134@ajou.ac.kr>
2021-04-07 17:26:55 +0900

    feat: createFlyer 함수 추가

--- PDFKitExample/PDFKitExample/PDFCreator.swift
+++ PDFKitExample/PDFKitExample/PDFCreator.swift
@@ -9,5 +9,28 @@ import UIKit
 import PDFKit
 
 class PDFCreator: NSObject {
+    func createFlyer() -> Data {
+        let pdfMetaData = [
+            kCGPDFContextCreator: "Flyer Builder",
+            kCGPDFContextAuthor: "songjureu"
+        ]
+        let format = UIGraphicsPDFRendererFormat()
+        format.documentInfo = pdfMetaData as [String: Any]
+        
+        let pageWidth = 8.5 * 72.0
+        let pageHeight = 11 * 72.0
+        let pageRect = CGRect(x: 0, y: 0, width: pageWidth, height: pageHeight)
+        
+        let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
+        let data = renderer.pdfData { context in
+            context.beginPage()
+            let attributes = [
+                NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 72)
+            ]
+            let text = "하이 헬로우 안녕"
+            text.draw(at: CGPoint(x: 0, y: 0), withAttributes: attributes)
+        }
+        return data
+    }
     
 }
