f27152d9605973d31bf36aedb94da80fa07962fb
songjucho <pinetree134@ajou.ac.kr>
2021-04-07 17:46:43 +0900

    feat: createFlyer 함수에 주석 추가

--- PDFKitExample/PDFKitExample/PDFCreator.swift
+++ PDFKitExample/PDFKitExample/PDFCreator.swift
@@ -9,7 +9,9 @@ import UIKit
 import PDFKit
 
 class PDFCreator: NSObject {
+    // pdf 를 만드는 함수
     func createFlyer() -> Data {
+        // 문서 이름과 저자를 지정한 메타데이터를 만들고 포맷을 생성해 documentInfo에 할당
         let pdfMetaData = [
             kCGPDFContextCreator: "Flyer Builder",
             kCGPDFContextAuthor: "songjureu"
@@ -17,13 +19,18 @@ class PDFCreator: NSObject {
         let format = UIGraphicsPDFRendererFormat()
         format.documentInfo = pdfMetaData as [String: Any]
         
+        // 8.5 * 11 인치는 standard U.S. letter size임
         let pageWidth = 8.5 * 72.0
         let pageHeight = 11 * 72.0
         let pageRect = CGRect(x: 0, y: 0, width: pageWidth, height: pageHeight)
         
+        // 지정한 사이즈와 정보가 담긴 포맷으로 렌더러를 생성해줌
         let renderer = UIGraphicsPDFRenderer(bounds: pageRect, format: format)
+        // pdf 로 만들 내용을 포함하는 블럭, 안에서 그리는 것들이 곧 pdf 내용이 됨
         let data = renderer.pdfData { context in
+            // draw 하기 전에 무조건 호출해주어야함. 만약 여러 페이지의 pdf를 만들려면 더 호출해도 됨.
             context.beginPage()
+            // string draw 하는 부분
             let attributes = [
                 NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 72)
             ]
